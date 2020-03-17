c36c19ba8289928c6c4c8991cd2e5b7e91470e27
Afua <afuadanquah@gmail.com>
2020-03-17 06:07:51 +0000

    sql update

--- superglobals.sql
+++ superglobals.sql
@@ -1,48 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 11, 2020 at 07:05 AM
--- Server version: 10.1.25-MariaDB
--- PHP Version: 7.0.21
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Database: `superglobals`
 --
 
--- --------------------------------------------------------
-
---
--- Table structure for table `appointments booking`
---
-
-CREATE TABLE `appointments booking` (
-  `AppointmentID` int(11) NOT NULL,
-  `CustomerID` int(11) DEFAULT NULL,
-  `TreatmentID` int(11) DEFAULT NULL,
-  `Date` date NOT NULL,
-  `StartTime` time NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `appointments booking`
---
-
-INSERT INTO `appointments booking` (`AppointmentID`, `CustomerID`, `TreatmentID`, `Date`, `StartTime`) VALUES
-(2, 1, 1, '2020-03-12', '14:00:00'),
-(3, 2, 2, '2020-03-13', '15:00:00');
+CREATE DATABASE Superglobals39;
 
 -- --------------------------------------------------------
 
@@ -50,24 +9,28 @@ INSERT INTO `appointments booking` (`AppointmentID`, `CustomerID`, `TreatmentID`
 -- Table structure for table `customers`
 --
 
-CREATE TABLE `customers` (
-  `CustomerID` int(11) NOT NULL,
-  `Username` varchar(25) NOT NULL,
-  `Password` varchar(20) NOT NULL,
-  `FirstName` varchar(50) NOT NULL,
-  `LastName` varchar(50) NOT NULL,
-  `DateofBirth` date DEFAULT NULL,
-  `PhoneNo` int(11) DEFAULT NULL,
-  `Email` char(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE customers (
+  CustomerID int NOT NULL AUTO_INCREMENT,
+  Username varchar(25) NOT NULL,
+  Password varchar(20) NOT NULL,
+  FirstName varchar(50) NOT NULL,
+  LastName varchar(50) NOT NULL,
+  DateofBirth date DEFAULT NULL,
+  PhoneNo varchar(15) DEFAULT NULL,
+  Email varchar(50) NOT NULL,
+  PRIMARY KEY (CustomerID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
 
 --
 -- Dumping data for table `customers`
 --
 
-INSERT INTO `customers` (`CustomerID`, `Username`, `Password`, `FirstName`, `LastName`, `DateofBirth`, `PhoneNo`, `Email`) VALUES
-(1, 'danquaha', 'abc123efg', 'Fia', 'Danni', '2020-03-17', 92832013, 'fiadanni@gmail.com'),
-(2, 'jackson123', 'password', 'Jack', 'Doe', '1990-07-22', 9298311, '');
+INSERT INTO customers (Username, Password, FirstName, LastName, DateofBirth, PhoneNo, Email) VALUES
+('mauraw', 'wmaura', 'Maura', 'Watkinson', '1990-03-17', '0796253372', 'mw@gmail.com'),
+('afuad', 'dafua', 'Afua', 'Danquah', '1991-04-17', '07827323422', 'abd@gmail.com'),
+('tomii', 'itomi', 'Tomi', 'Ibirogba', '1992-05-17', '07732153532', 'ti@gmail.com'),
+('zaraz', 'zzara', 'Zara', 'Zatzu', '1993-06-17', '07832847283', 'zz@gmail.com'),
+('ciarap', 'pciara', 'Ciara', 'Pimley', '1994-07-17', ' ', 'cp@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -75,44 +38,65 @@ INSERT INTO `customers` (`CustomerID`, `Username`, `Password`, `FirstName`, `Las
 -- Table structure for table `treatments`
 --
 
-CREATE TABLE `treatments` (
-  `TreatmentID` int(11) NOT NULL,
-  `TreatmentName` varchar(100) NOT NULL,
-  `Duration` int(11) NOT NULL,
-  `Price` decimal(10,0) NOT NULL,
-  `Description` text
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE Treatments (
+   TreatmentID int NOT NULL AUTO_INCREMENT,
+   TreatmentName varchar(100) NOT NULL,
+   Duration int(11) NOT NULL,
+   Price double default 0 NOT NULL,
+   Description text,
+   PRIMARY KEY (TreatmentID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
 
 --
 -- Dumping data for table `treatments`
 --
 
-INSERT INTO `treatments` (`TreatmentID`, `TreatmentName`, `Duration`, `Price`, `Description`) VALUES
-(1, 'Facial', 45, '20', 'Massaging to the face'),
-(2, 'Laser', 60, '50', 'Hair Removal');
+INSERT INTO treatments (TreatmentName, Duration, Price, Description) VALUES
+('Massage', '45', '20', 'Deep Tissue Massage'),
+('Laser', '60', '50', 'Hair Removal - Face'),
+('Facial', '60', '40', 'Hydrafacial' );
 
 --
--- Indexes for dumped tables
+-- Table structure for table `appointments booking`
 --
 
+CREATE TABLE appointments (
+  AppointmentID int NOT NULL AUTO_INCREMENT,
+  CustomerID int,
+  TreatmentID int,
+  Date date NOT NULL,
+  StartTime time NOT NULL,
+  PRIMARY KEY (AppointmentID),
+  FOREIGN KEY (CustomerID) REFERENCES customers(CustomerID),
+  FOREIGN KEY (TreatmentID) REFERENCES treatments(TreatmentID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+
+--
+-- Dumping data for table `appointments booking`
+--
+
+INSERT INTO appointments (CustomerID, TreatmentID, Date, StartTime) VALUES
+('4', '3', '2020-03-12', '140000'),
+('1', '2', '2020-03-13', '150000'),
+('2', '1', '2020-03-15', '160000');
 --
 -- Indexes for table `appointments booking`
 --
-ALTER TABLE `appointments booking`
-  ADD PRIMARY KEY (`AppointmentID`),
-  ADD KEY `FK` (`CustomerID`,`TreatmentID`);
+-- ALTER TABLE `appointments booking`
+-- ADD PRIMARY KEY (`AppointmentID`),
+-- ADD KEY `FK` (`CustomerID`,`TreatmentID`);
 
 --
 -- Indexes for table `customers`
 --
-ALTER TABLE `customers`
-  ADD PRIMARY KEY (`CustomerID`);
+-- ALTER TABLE `customers`
+-- ADD PRIMARY KEY (`CustomerID`);
 
 --
 -- Indexes for table `treatments`
 --
-ALTER TABLE `treatments`
-  ADD PRIMARY KEY (`TreatmentID`);
+-- ALTER TABLE `treatments`
+-- ADD PRIMARY KEY (`TreatmentID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -121,18 +105,18 @@ ALTER TABLE `treatments`
 --
 -- AUTO_INCREMENT for table `appointments booking`
 --
-ALTER TABLE `appointments booking`
-  MODIFY `AppointmentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+-- ALTER TABLE `appointments booking`
+-- MODIFY `AppointmentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `customers`
 --
-ALTER TABLE `customers`
-  MODIFY `CustomerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+-- ALTER TABLE `customers`
+-- MODIFY `CustomerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `treatments`
 --
-ALTER TABLE `treatments`
-  MODIFY `TreatmentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
+-- ALTER TABLE `treatments`
+-- MODIFY `TreatmentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
